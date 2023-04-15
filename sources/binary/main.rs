f4ac9d5bd3d6a8b21e78f7e249d37ac0145f9759
MrPhnix <76911907+MrPhnix@users.noreply.github.com>
2023-04-15 15:19:13 +0200

    Initialize the rust project

---
+++ sources/binary/main.rs
@@ -0,0 +1,11 @@
+// ╔════════════════════════════════════════════════════════╗
+// ║ main.rs                                                ║
+// ╠════════════════════════════════════════════════════════╣
+// ║         This file is part of the ROGON ENGINE.         ║
+// ║ Licensed under the Mozilla Public License Version 2.0. ║
+// ╚════════════════════════════════════════════════════════╝
+
+fn main()
+{
+	println!("Hello, world!");
+}
